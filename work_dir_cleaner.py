31f5d8e6f2d4f4da1a540cc4bdbbb83f2eea86f6
Filcuk <filcuk@users.noreply.github.com>
2021-11-29 11:01:31 +0000

    Update work_dir_cleaner.py

--- work_dir_cleaner.py
+++ work_dir_cleaner.py
@@ -1,1 +1,24 @@
 import os
+from os import walk
+import fnmatch
+
+# r- prefix causes string to be treated as literal 
+userDir = r'C:\Users\FilipK'
+downDir = userDir + r'\Downloads'
+exprDir = userDir + r'C:\Users\FilipK\Desktop\Export\'
+scrCapsDir = exprDir + r'\Screenshots'
+
+scrCapsPattern = r'Screenshot ????-??-?? ??????.png'
+
+f = []
+
+
+for file in os.listdir(exprDir):
+    if fnmatch.fnmatch(file, scrCapsPattern):
+        f.extend(file)
+
+print(f)
+
+
+
+input()
